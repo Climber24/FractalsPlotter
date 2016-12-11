47b6bc5a6d5a14fccd1bd23ef5e17b54c602a6c4
LaHesis <cryptograph2013@yandex.ru>
2016-12-11 20:54:53 +0700

    initialCommit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnKoch = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolStripDelayTxtBox = new System.Windows.Forms.ToolStripTextBox();
+            this.pnKoch.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnKoch
+            // 
+            this.pnKoch.Controls.Add(this.toolStrip1);
+            this.pnKoch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnKoch.Location = new System.Drawing.Point(3, 3);
+            this.pnKoch.Name = "pnKoch";
+            this.pnKoch.Size = new System.Drawing.Size(845, 414);
+            this.pnKoch.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripDelayTxtBox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(845, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(114, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.HotTrack = true;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(859, 446);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pnKoch);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(851, 420);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(851, 420);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStripDelayTxtBox
+            // 
+            this.toolStripDelayTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.toolStripDelayTxtBox.Name = "toolStripDelayTxtBox";
+            this.toolStripDelayTxtBox.Size = new System.Drawing.Size(100, 25);
+            this.toolStripDelayTxtBox.Text = "Задержка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 446);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.pnKoch.ResumeLayout(false);
+            this.pnKoch.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel pnKoch;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripTextBox toolStripDelayTxtBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nakov.TurtleGraphics;
 
 namespace FractalsPlotter
 {
@@ -15,6 +16,71 @@ namespace FractalsPlotter
         public Form1()
         {
             InitializeComponent();
+            Turtle.Init(pnKoch);
+        }
+
+        private void DrawKoch_Click(object sender, EventArgs e)
+        {
+            // Assign a delay to visualize the drawing process 
+            Turtle.Delay = 200;
+
+            // Draw a equilateral triangle 
+            Turtle.Rotate(30);
+            Turtle.Forward(200);
+            Turtle.Rotate(120);
+            Turtle.Forward(200);
+            Turtle.Rotate(120);
+            Turtle.Forward(200);
+
+            // Draw a line in the triangle 
+            Turtle.Rotate(-30);
+            Turtle.PenUp();
+            Turtle.Backward(50);
+            Turtle.PenDown();
+            Turtle.Backward(100);
+            Turtle.PenUp();
+            Turtle.Forward(150);
+            Turtle.PenDown();
+            Turtle.Rotate(30);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 10;
+            Koch.DrawSnowflake(2);
+        }
+    }
+
+    class Koch
+    {
+        public static int segment = 5;
+        public static void DrawSnowflake(int n)
+        {
+            Turtle.PenSize = 1;
+            Turtle.MoveTo(0, 0);
+            Turtle.PenDown();
+
+            snowflakeOneSide(n);
+            //Turtle.Rotate(120);
+            //snowflakeOneSide(n);
+        }
+
+        static void snowflakeOneSide(int n)
+        {
+            if (n == 0)
+            {
+                Turtle.Forward(segment);
+            }
+            else
+            {
+                snowflakeOneSide(n - 1);
+                Turtle.Rotate(-60);
+                snowflakeOneSide(n - 1);
+                Turtle.Rotate(120);
+                snowflakeOneSide(n - 1);
+                Turtle.Rotate(-60);
+                snowflakeOneSide(n - 1);
+            }
         }
     }
 }
